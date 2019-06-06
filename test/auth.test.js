b950646a3d5c03be98d3e0d8db41a1fa0393bf30
Ian <ianbingham24@gmail.com>
2019-06-05 20:41:23 -0700

    renamed stuff in auth.test

--- test/auth.test.js
+++ test/auth.test.js
@@ -6,36 +6,35 @@ const { createUser, login } = require('./utils/util');
 
 describe('Signup', () => {
   it('should error - password mistmatch', async () => {
-    const res = await createUser('test1', 'test1', 'notTest1');
+    const createUserRes = await createUser('authUser1', 'authUser1', 'notAuthUser1');
 
-    expect(res.status).to.eq(400);
-    expect(res.body).to.have.property('error');
-    expect(res.body.error).to.eq('passwords do not match');
+    expect(createUserRes.status).to.eq(400);
+    expect(createUserRes.body).to.have.property('error');
+    expect(createUserRes.body.error).to.eq('passwords do not match');
   });
 
   it('should sign up w/ hidden passwordHash', async () => {
-    const res = await createUser('test1', 'test1', 'test1');
+    const createUserRes = await createUser('authUser1', 'authUser1', 'authUser1');
 
-    expect(res.status).to.eq(200);
-    expect(res.body).to.have.property('username');
-    expect(res.body).to.not.have.property('passwordHash');
+    expect(createUserRes.status).to.eq(200);
+    expect(createUserRes.body).to.have.property('username');
+    expect(createUserRes.body).to.not.have.property('passwordHash');
   });
 
   it('should not sign up - duplicate user', async () => {
-    const res = await createUser('test1', 'test1', 'test1');
+    const createUserRes = await createUser('authUser1', 'authUser1', 'authUser1');
 
-    expect(res.status).to.eq(400);
-    expect(res.body).to.have.property('error');
-    expect(res.body.error).to.eq('user already exists');
+    expect(createUserRes.status).to.eq(400);
+    expect(createUserRes.body).to.have.property('error');
+    expect(createUserRes.body.error).to.eq('user already exists');
   });
 });
 
-
 describe('Login', () => {
   it('should login', async () => {
-    const res = await login('test1', 'test1');
+    const loginRes = await login('authUser1', 'authUser1');
 
-    expect(res.status).to.eq(200);
+    expect(loginRes.status).to.eq(200);
   });
 
 });
